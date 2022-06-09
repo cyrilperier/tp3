b490a240d1c4a3c50f9765f68324822b6adc26b8
Launois remy <remy.launois@gmail.com>
2022-06-09 13:32:03 -0400

    Class test

---
+++ src/RegisterTest.java
@@ -0,0 +1,19 @@
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Project: tp3
+ * Package: PACKAGE_NAME
+ */
+public class RegisterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
