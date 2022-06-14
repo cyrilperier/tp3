d6a912c1a23e0bf2f2087404e9376c96bdfa3841
cyrilperier <cyril.perier@epfedu.fr>
2022-06-14 14:05:19 -0400

    tests 1 et 2 et 3

--- src/RegisterTest.java
+++ src/RegisterTest.java
@@ -50,7 +50,18 @@ public class RegisterTest {
     @Test
     public void verificationPrix_PrixFractionaireAvecCUPCommencantParDeux_PrixCorrecte(){
 
-        grocery.add(new Item(Upc.generateCode("12345678901"), "Bananas", 1, 1.5));
+        List<Item> grocery = new ArrayList<Item>();
+
+        grocery.add(new Item(Upc.generateCode("22804918500"), "Beef", 0.5, 5.75));
+
+        // Print receipt
+        System.out.println(register.print(grocery));
+
+    }
+
+    @Test
+    public void verificationList_BonNombreArticle_ListValide(){
+
         List<Item> grocery = new ArrayList<Item>();
 
         grocery.add(new Item(Upc.generateCode("22804918500"), "Beef", 0.5, 5.75));
@@ -64,6 +75,8 @@ public class RegisterTest {
 
     @Test
     private void deuxMemeCUP_QuantitePositive_Invalid(){
+
+
     }
 
 }
