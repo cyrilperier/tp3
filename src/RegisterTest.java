d0f567125103f9e167c730b819c0e98cfe53805d
Carla <carla.faboumy@epfedu.fr>
2022-06-14 16:01:50 -0400

    NUMERO BIS

--- src/RegisterTest.java
+++ src/RegisterTest.java
@@ -97,7 +97,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISEE
-     *  1         V1 ∩ V2 ∩ v16                                                                       PRIX CORRECT        GROUPE ET INTERVALLE
+     *  1        V1 ∩ V2 ∩ v16                                                                       PRIX CORRECT        GROUPE ET INTERVALLE
      */
     @Test
     public void un_VerificationPrix_IntervalleMaxEtMinPourUnarticleEtPrixTotalNonNegatif_PrixCorrecte(){
@@ -113,7 +113,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISEE
-     *  2         V3                                      V10                                         PRIX CORRECT        SPECIFIQUE
+     *  2        V3                                      V10                                         PRIX CORRECT        SPECIFIQUE
      */
     @Test
     public void deux_VerificationPrix_PrixFractionaireAvecCUPCommencantParDeux_PrixCorrecte(){
@@ -129,7 +129,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISEE
-     *  3                         V4 ∩ V5                                                             LIST VALIDE         INTERVALLE
+     *  3                        V4 ∩ V5                                                             LIST VALIDE         INTERVALLE
      */
     @Test
     public void trois_VerificationList_BonNombreArticle_ListValide(){
@@ -145,7 +145,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISEE
-     *  4                                                 V6 ∩ V7                                     CUP VALIDE          SPECIFIQUE
+     *  4                                                V6 ∩ V7                                    CUP VALIDE          SPECIFIQUE
      */
     @Test
     public void quatre_VerificationCUP_BonNombreDeCaractereEtDouziemeRespecteLaRegle_CUPValide(){
@@ -161,7 +161,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISE
-     *  5                                                 V8                                          CUP VALIDE
+     *  5                                                V8                                          CUP VALIDE
      */
     @Test
     public void cinq_VerificationCUP_DeuxProduitAvecMemeCUPMaisUnQuantiteNegative_CUPValide(){
@@ -178,7 +178,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISE
-     *  6                                                 V9 ∩ V10                                    CUP VALIDE          GROUPE
+     *  6                                                V9 ∩ V10                                    CUP VALIDE          GROUPE
      */
     @Test
     public void six_VerificationCUP_CupCommenceParLeBonChiffreParRapportAuProduit_CUPValide(){
@@ -213,7 +213,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISEE
-     *  8                                                 V9                          V13 ∩ V14 ∩ V15 COUPON VALIDE       GROUPE
+     *  8                                                V9                          V13 ∩ V14 ∩ V15 COUPON VALIDE       GROUPE
      */
     @Test
     public void huit_ApplicationDeCoupon_PrixCouponInferieurPrixTotalEtSuperieurAZero_CouponValide() {
@@ -285,7 +285,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISEE
-     *  13                                                I8                                          MSG ERREUR          SPECIFIQUE
+     *  13                                               I8                                          MSG ERREUR          SPECIFIQUE
      */
     @Test(expected = InvalidUpcException.InvalidCheckDigitException.class)
     public void treize_CheckDigit_LastNumber_invalid() {
@@ -295,7 +295,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISEE
-     *  14                                                I9 ∪ I14                                    MSG ERREUR          GROUPE
+     *  14                                               I9 ∪ I14                                    MSG ERREUR          GROUPE
      */
     @Test(expected = Register.DuplicateItemException.class)
     public void quatorze_DeuxMemesCUP_QuantitePositive_Invalid() {
@@ -306,7 +306,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISEE
-     *  15                                                                            I15 ∪ I16       Coupon suivant      GROUPE ET INTERVALLE
+     *  15                                                                           I15 ∪ I16       Coupon suivant      GROUPE ET INTERVALLE
      */
     @Test()
     public void quinze_ValeurCoupon_SuperieurPrixTotOuInf0_DeuxiemeCoupon() {
@@ -319,7 +319,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISEE
-     *  16                                                            V11 ∩ I11                       Pas de rabais       GROUPE
+     *  16                                                           V11 ∩ I11                       Pas de rabais       GROUPE
      */
     @Test()
     public void seize_VerifArticle_Sup5EtPrixInf2_PasDeRabais() {
@@ -333,7 +333,7 @@ public class RegisterTest {
 
     /**
      * TEST      PRIX            LISTE DE PRODUIT        CUP         RABAIS          COUPON          RÉSULTAT            HEURISTIQUE UTILISEE
-     *  17                                                            V12 ∩ I10                       Pas de rabais       GROUPE
+     *  17                                                           V12 ∩ I10                       Pas de rabais       GROUPE
      */
     @Test()
     public void dix_sept_VerifArticle_Inf5EtPrixSup2_PasDeRabais() {
