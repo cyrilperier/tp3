894da8fc1c7de4bc3e10025ed85d6186260779a5
Launois remy <remy.launois@gmail.com>
2022-06-14 15:12:07 -0400

    TEst Remy

--- src/RegisterTest.java
+++ src/RegisterTest.java
@@ -1,10 +1,8 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import stev.kwikemart.Item;
-import stev.kwikemart.PaperRoll;
-import stev.kwikemart.Register;
-import stev.kwikemart.Upc;
+import stev.kwikemart.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -72,11 +70,46 @@ public class RegisterTest {
     }
 
 
+    @Test(expected = InvalidUpcException.InvalidCheckDigitException.class)
+    public void checkDigit_LastNumber_invalid() {
+        grocery.add(new Item("036000291451", "Bananas", 1, 1.5));
 
-    @Test
-    public void deuxMemeCUP_QuantitePositive_Invalid(){
-
-
+        System.out.println(register.print(grocery));
     }
+    @Test(expected = Register.DuplicateItemException.class)
+    public void deuxMemeCUP_QuantitePositive_Invalid() {
+        grocery.add(new Item(Upc.generateCode("12345678901"), "Bananas", 1, 1.5));
+        grocery.add(new Item(Upc.generateCode("12345678901"), "Bananas", 1, 1.5));
+        System.out.println(register.print(grocery));
+    }
+
+    @Test()
+    public void valeurCoupon_SuperieurPrixTot_DeuxiemeCoupon() {
+        grocery.add(new Item(Upc.generateCode("12345678901"), "Bananas", 2, 0.5));
+        grocery.add(new Item(Upc.generateCode("12345677901"), "Apple", 1, 1.5));
+        grocery.add(new Item(Upc.generateCode("54323432343"), "Rabais Fruits", 1, 3));
+        grocery.add(new Item(Upc.generateCode("54323432443"), "Rabais 1$", 1, 2));
+        Assert.assertTrue(register.print(grocery).contains("Rabais 1$"));
+    }
+
+    @Test()
+    public void verifArticle_Sup5EtPrixInf2_PasDeRabais() {
+        grocery.add(new Item(Upc.generateCode("12345678901"), "Bananas", 2, 0.5));
+        grocery.add(new Item(Upc.generateCode("12345677901"), "Apple", 1, 0.2));
+        grocery.add(new Item(Upc.generateCode("12345577901"), "Green-Apple", 1, 0.5));
+        grocery.add(new Item(Upc.generateCode("12345476901"), "Apple-juice", 1, 0.1));
+        grocery.add(new Item(Upc.generateCode("12344477901"), "Oranges", 1, 0.0));
+        Assert.assertFalse(register.print(grocery).contains("Rebate for 5 items  "));
+    }
+
+    @Test()
+    public void verifArticle_Inf5EtPrixSup2_PasDeRabais() {
+        grocery.add(new Item(Upc.generateCode("12345678901"), "Bananas", 2, 0.5));
+        grocery.add(new Item(Upc.generateCode("12345677901"), "Apple", 1, 0.5));
+        grocery.add(new Item(Upc.generateCode("12345577901"), "Green-Apple", 1, 0.5));
+        grocery.add(new Item(Upc.generateCode("12345476901"), "Apple-juice", 1, 1.1));
+        Assert.assertFalse(register.print(grocery).contains("Rebate for 5 items  "));
+    }
+
 
 }
